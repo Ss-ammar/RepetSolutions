86621af8705b0ba4b2d7924301c2ecedddf8e309
s_ammar <soufianeammar.96@gmail.com>
2022-03-02 15:25:34 +0100

    done

---
+++ 1.2ForLoops.py
@@ -0,0 +1,25 @@
+"""
+
+@author: AMMAR
+
+"""
+#Print the numbers 0 to 100
+for i in range (0,101):
+    print(i),
+    
+#Print the numbers 0 to 100 that are divisible by 7
+
+for i in range (0,101):
+    if i%7 == 0 :
+        print(i),
+
+#Print the numbers 1 to 100 that are divisible by 5 but not by 3
+
+for i in range (0,101):
+    if i%5 == 0: 
+        if i%3 != 0:
+            print(i)
+
+
+
+    
