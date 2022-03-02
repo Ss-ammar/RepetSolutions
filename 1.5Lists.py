16bd19de0fdd4c6db8bc0ea7ca2397013a914dfb
s_ammar <soufianeammar.96@gmail.com>
2022-03-02 15:26:59 +0100

    done

---
+++ 1.5Lists.py
@@ -0,0 +1,9 @@
+"""
+
+@author: AMMAR
+
+"""
+Countries = ['Belgium', 'Netherlands', 'Luxembourg']
+Length = [len(Countries[0]), len(Countries[1]), len(Countries[2]) ]
+for i in range(3):
+    print ("The length of", Countries[i], "is", Length[i])
